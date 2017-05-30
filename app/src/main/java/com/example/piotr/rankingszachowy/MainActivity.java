c2e77e82bfa267a2e9f6702656810280215c5a6d
rozpierog <rozpierog@gmail.com>
2017-05-30 15:07:31 +0200

    Fixed editable mode in playerFragment. Icon in imgButton changes when functionality changes. App is fixed in portrait oriantation.

--- app/src/main/java/com/example/piotr/rankingszachowy/MainActivity.java
+++ app/src/main/java/com/example/piotr/rankingszachowy/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.piotr.rankingszachowy;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
 
 
